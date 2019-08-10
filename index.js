33fc5c6d84b53fd7a8e96e622bd3742dbf3cf419
Sandip Babu <sbabu@sandips-imac.lan>
2019-08-09 22:36:09 -0700

    sends a meme in discord channel from r/animemes

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix, token } = require('./config.json');
 const fetch = require("node-fetch");
+var meme ="";
+
 
 
 client.once('ready', () => {
   console.log('Ready!');
   
 });
+
 
 
 client.on('message', message => {
@@ -20,41 +23,38 @@ client.on('message', message => {
   }
 
   if (message.content.startsWith(`${prefix}meme`)){
+    
 
+    
     fetch("https://www.reddit.com/r/Animemes/.json")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(jsonObj) {
-           var data = jsonObj.data;
-           var children = data.children;
-           var imgUrl = new Array;
+      .then(function(response) {
+         return response.json();
+       })
+      .then(function(jsonObj) {
 
-           for (var i = 0; i < children.length; i++) {
-             var childData = children[i].data;
-             var imgPreview = childData.preview;
-             var imgArray = imgPreview.images;
-             var source = imgArray[0].source;
-             imgUrl[i] = source.url;
+        var data = jsonObj.data;
+        var children = data.children;
+        var imgUrl = new Array;
 
+        for (var i = 0; i < children.length; i++) {
+         var childData = children[i].data;
+        imgUrl[i]= childData.url;
+        };    
+        var rng = Math.floor(Math.random() * children.length) + 3;
+        var imgFile = imgUrl[rng];
+        console.log(imgFile);
 
-             
+        console[imgFile];
+        message.channel.send("",{  
+        
+          file: imgFile
+      
+        });
+    
+      });
 
-           };
-           var rng = Math.floor(Math.random() * children.length) + 3;
-           var imgFile = imgUrl[rng];
-          
-           console.log(file);
-           console.log(rng);
-           console.log(imgFile);
-
-          
-            console.log(children.length)
-            
-          });
-
-    message.channel.send("MEMEs")
-  }
+   
+    }
 
   
   });
