c51c5c030a3f1eb1dc99a364ff7f6489a6c52d92
KaleyMoro <csrijan10@gmail.com>
2019-08-14 23:13:15 -0700

    server details

--- commands/general/serverinfo.js
+++ commands/general/serverinfo.js
@@ -1,14 +1,26 @@
+const Discord = require("discord.js")
 module.exports = {
     config: {
         name: "serverinfo",
-        aliases: ["s"],
+        aliases: ["s","si","server"],
         usage: "",
         category: "general",
-        description: "",
+        description: "Shows the info of the current guild",
         accessableby: ""
 	},
 	run: async (client, message, args) => {
-		//code here
+        let sEmbed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle("Server Info")
+        .setThumbnail(message.guild.iconURL)
+        .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
+        .addField("**Guild Name:**", `${message.guild.name}`, true)
+        .addField("**Guild Owner:**", `${message.guild.owner}`, true)
+        .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+        .addField("**Role Count:**", `${message.guild.roles.size}`, true)
+        .setTimestamp()
+        .setFooter(`${message.guild.me.displayName}`, client.user.displayAvatarURL);
+        message.channel.send({embed: sEmbed});
 
 	}
 }
