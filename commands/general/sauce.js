5f440ce93b87aab53f3690fe4aac3b1f398e57bc
KaleyMoro <csrijan10@gmail.com>
2019-09-10 12:37:26 -0700

    doesnt show the null/undefined value added Drawr,Deviant art and Pixiv Details

--- commands/general/sauce.js
+++ commands/general/sauce.js
@@ -26,7 +26,7 @@ module.exports = {
                     })
                     .then(function (jsonObj) {
                         var c = 0;
-                        var d = 0;
+                       
                         var results = jsonObj.results;
                         for (var call of results) {
 
@@ -38,10 +38,21 @@ module.exports = {
                             var mainValue = call.data
                             var source = mainValue.source;
                             var year = mainValue.year;
-                            var pixiv_id = mainValue.pixiv_id;
+                            var pixiv_id = mainValue.pixiv_id; //on pixiv
+                            var member_name= mainValue.member_name;
+                            var title = mainValue.title; //of pixiv
                             var danbooru_id = mainValue.danbooru_id;
                             var material = mainValue.material; //on danbooru
                             var character = mainValue.characters; //on danbooru
+                            var aniDb_id = mainValue.anidb_id;
+                            var part = mainValue.part;
+                            var est_time = mainValue.est_time;
+                            var drawr_id = mainValue.drawr_id //on drawr
+                            var da_id = mainValue.da_id //Deviant Art
+                            var author_name = mainValue.author_name //Deviant art Author Name
+                            var author_url = mainValue.author_url // Deviantart Author URL
+
+                            var creator = mainValue.creator //creater on booru site
 
 
                             var percentage = parseFloat(similarity);
@@ -65,10 +76,26 @@ module.exports = {
                                 .setThumbnail(thumbnail)
                                 // .addBlankField()
                                 .addField("**Similarity:**", similarity)
-                                .addField("source:", source)
-                                .addField("year:", year)
-                                .addField("index-name:", index_name)
-                                .addField("External Url:", url)
+                                if(title != undefined){ embed.addField("**Title:**", title)}
+
+                                if(source != undefined){ embed.addField("**Source:**", source)}
+                                if(creator != undefined){ embed.addField("**Creator:**", creator)}
+                                if(pixiv_id != undefined){ embed.addField("**Pixiv ID:**", pixiv_id)}
+                                if(member_name != undefined){ embed.addField("**Member Name:**", member_name)}
+                                if(drawr_id != undefined){ embed.addField("**Drawr ID**", drawr_id)}
+                                if(da_id != undefined){ embed.addField("**Deviantart ID:**", da_id)}
+                                if(author_name != undefined){ embed.addField("**Author Name**", author_name)}
+                                if(author_url != undefined){ embed.addField("**Author url**", author_url)}
+                                if(danbooru_id != undefined){ embed.addField("**Danbooru ID:**", danbooru_id)}
+                                if(material != undefined){ embed.addField("**Material:**", material)}
+                                if(character != undefined){ embed.addField("**Character:**", character)}
+                                if(aniDb_id != undefined){ embed.addField("**AniDB ID:**", aniDb_id)}
+                                if(year != undefined){ embed.addField("**Year:**", year)}
+                                if(part != undefined){ embed.addField("**Part:**", part)}
+                                if(est_time != undefined){ embed.addField("**Estimated Time:**", est_time)}
+                                if(index_name != undefined){ embed.addField("**Index:**", index_name)}
+                                if(url != undefined){ embed.addField("**External Url:**", url)}
+                               
                             message.channel.send({ embed: embed });
 
                             c = c + 1;
