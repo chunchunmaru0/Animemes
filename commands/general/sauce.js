57353ec0ffe0a6199258d71561f95f2af67634d3
KaleyMoro <csrijan10@gmail.com>
2019-09-10 00:45:36 -0700

    Sends a message if there is no picture to find SAUCE of

--- commands/general/sauce.js
+++ commands/general/sauce.js
@@ -13,67 +13,71 @@ module.exports = {
         //code here
 
         var Attachment = (message.attachments).array();
-        Attachment.forEach(function (attachment) {
-            console.log(attachment.url);
+        if (Attachment[0] == null) {
+            message.channel.send("Please add an Image alongside with SAUCE command to find the sauce of the picture!")
+        } else {
+            //console.log(Attachment)
+            Attachment.forEach(function (attachment) {
+                console.log(attachment.url);
 
-            fetch("https://saucenao.com/search.php?output_type=2&url=" + attachment.url)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (jsonObj) {
-                    var c=0;
-                    var d=0;
-                    var results = jsonObj.results;
-                    for (var call of results) {
-                        
-                        console.log(call)
-                        var data = call.header
-                        var similarity = data.similarity;
-                        var thumbnail = data.thumbnail;
-                        var index_name = data.index_name;
-                        var mainValue = call.data
-                        var source = mainValue.source;
-                        var year = mainValue.year;
-                        var pixiv_id= mainValue.pixiv_id;
-                        var danbooru_id= mainValue.danbooru_id;
-                        var material = mainValue.material; //on danbooru
-                        var character = mainValue.characters; //on danbooru
-                        
+                fetch("https://saucenao.com/search.php?output_type=2&url=" + attachment.url)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (jsonObj) {
+                        var c = 0;
+                        var d = 0;
+                        var results = jsonObj.results;
+                        for (var call of results) {
 
-                        var percentage = parseFloat(similarity);
-                        
-                        if (percentage <= 50) { 
-                            
-                            if(c != 0){
-                                // message.channel.send("thats it")
-                                console.log("SAUCE FOUND!")
-                            } else {
-                                message.channel.send("Sadly, there is no SAUCE <:PePe_hands:595077130665197579>")
+                            //console.log(call)
+                            var data = call.header
+                            var similarity = data.similarity;
+                            var thumbnail = data.thumbnail;
+                            var index_name = data.index_name;
+                            var mainValue = call.data
+                            var source = mainValue.source;
+                            var year = mainValue.year;
+                            var pixiv_id = mainValue.pixiv_id;
+                            var danbooru_id = mainValue.danbooru_id;
+                            var material = mainValue.material; //on danbooru
+                            var character = mainValue.characters; //on danbooru
+
+
+                            var percentage = parseFloat(similarity);
+
+                            if (percentage <= 50) {
+
+                                if (c != 0) {
+                                    // message.channel.send("thats it")
+                                    console.log("SAUCE FOUND!")
+                                } else {
+                                    message.channel.send("Sadly, there is no SAUCE <:PePe_hands:595077130665197579>")
+                                }
+                                break;
                             }
-                            break; }
-                        var url = mainValue.ext_urls;
-                        console.log("similarity:", similarity, "source:", source, "year:", year, "index-name:", index_name, "External Url:", url);
+                            var url = mainValue.ext_urls;
+                            console.log("similarity:", similarity, "source:", source, "year:", year, "index-name:", index_name, "External Url:", url);
 
-                        const embed = new Discord.RichEmbed()
-                            .setColor("#0099ff")
-                            .setTitle("here's the SAUCE: ")
-                            .setThumbnail(thumbnail)
-                            // .addBlankField()
-                            .addField("**Similarity:**", similarity)
-                            .addField("source:", source)
-                            .addField("year:", year)
-                            .addField("index-name:", index_name)
-                            .addField("External Url:", url)
-                        message.channel.send({ embed: embed });
+                            const embed = new Discord.RichEmbed()
+                                .setColor("#0099ff")
+                                .setTitle("here's the SAUCE: ")
+                                .setThumbnail(thumbnail)
+                                // .addBlankField()
+                                .addField("**Similarity:**", similarity)
+                                .addField("source:", source)
+                                .addField("year:", year)
+                                .addField("index-name:", index_name)
+                                .addField("External Url:", url)
+                            message.channel.send({ embed: embed });
 
-                        console.log("Value of C is ", c);
-                        console.log("value of D is ", d);
-                        c=c+1;
-                        d=d+1;
-                        
-                    }
-                })
-        })
+                            c = c + 1;
 
+                        }
+                    })
+
+            })
+
+        }
     }
 }
